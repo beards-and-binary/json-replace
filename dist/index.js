3577fff10b254152205cf7cb710ad1da2bf79b81
Benjamin Millhouse <ben.millhouse@teamcentral.ai>
2023-02-11 23:04:10 -0500

    run prepare

--- dist/index.js
+++ dist/index.js
@@ -2868,6 +2868,7 @@ async function run() {
         copy[kv.key] = kv.value;
       }
     });
+    core.info('All updates completed successfully');
     core.setOutput('json', copy);
   } catch (error) {
     core.setFailed(error.message);
