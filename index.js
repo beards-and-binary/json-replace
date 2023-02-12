ed9bd53e678d0d160703be9333d5fd15df1cb43e
Benjamin Millhouse <ben.millhouse@teamcentral.ai>
2023-02-11 22:14:07 -0500

    v1 release

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const parseJson = require('./parse-json');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const json = core.getInput('json', { required: true });
+    const properties = core.getMultilineInput('properties', { required: true });
+    const values = core.getMultilineInput('values', { required: true });
+    core.debug(`passed json: ${json}`);
+    core.debug(`passed properties: ${properties}`);
+    core.debug(`passed values: ${values}`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const copy = parseJson(json);
+    if (properties.length !== values.length) {
+      throw new RangeError('The number of passed properties and values does not match');
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info('All inputs are valid.  Performing updates...');
+    const joined = properties.map((p, i) => ({ key: p, value: values[i] }));
+    joined.forEach((kv) => {
+      try {
+        const parsedValue = JSON.parse(kv.value);
+        core.debug(`Setting property ${kv.key} as ${parsedValue}`);
+        copy[kv.key] = parsedValue;
+      } catch (error) {
+        // If the value can't be parsed, just assign it directly
+        core.debug(`Could not parse value, setting property ${kv.key} as ${kv.value}`);
+        copy[kv.key] = kv.value;
+      }
+    });
+    core.setOutput('json', copy);
   } catch (error) {
     core.setFailed(error.message);
   }
